a18e7caf72867f5752aa6fe8def0940fffb9d4d2
GISELLE ROSS <GISELLEROSSTECH@GMAIL.COM>
2024-03-01 16:26:42 -0500

    Database complete

--- db/migration.sql
+++ db/migration.sql
@@ -1,20 +1,20 @@
-DROP TABLE item IF EXISTS   
-DROP TABLE reviews IF EXISTS  
+DROP TABLE IF EXISTS reviews; 
+DROP TABLE IF EXISTS item;
 
 CREATE TABLE item (
-    "id" SERIAL PRIMARY KEY,
-    "name" TEXT NOT NULL
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL
 );
+
 CREATE TABLE reviews (
-    "id" SERIAL PRIMARY KEY,
-    "rating" INTEGER NOT NULL,
-    "username" TEXT NOT NULL,
-    "date" DATE NOT NULL,
-    "title" TEXT NOT NULL,
-    "review_body" TEXT NOT NULL,
-    "recommended" BOOLEAN NOT NULL,
-    "helpful_yes" INTEGER NOT NULL,
-    "helpful_no" INTEGER NOT NULL,
-    "item_id" INTEGER NOT NULL,
-    FOREIGN KEY (item_id) REFERENCES Item(id) ON DELETE CASCADE
+    id SERIAL PRIMARY KEY,
+    rating INTEGER NOT NULL,
+    username TEXT NOT NULL,
+    date DATE NOT NULL,
+    title TEXT NOT NULL,
+    review_body TEXT NOT NULL,
+    recommended BOOLEAN NOT NULL,
+    helpful_yes INTEGER NOT NULL,
+    helpful_no INTEGER NOT NULL,
+    item_id INT NOT NULL REFERENCES item(id)
 );
