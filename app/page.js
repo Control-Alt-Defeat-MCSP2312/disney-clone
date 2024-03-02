261cfcae1c27e0fcee922b59e9c6b3edd7ef208a
DANIEL_GRADY <anakria977@gmail.com>
2024-03-02 10:27:22 -0500

    Implement first API hit to external database

--- app/page.js
+++ app/page.js
@@ -1,8 +1,6 @@
-
 export default function Home() {
+
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+    <h1>Hello World</h1>
   );
 }
---
+++ db/db.js
@@ -0,0 +1,25 @@
+import { config } from 'dotenv';
+import { Pool } from 'pg';
+config();
+
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false,
+    }
+});
+
+export async function query(sql, params = []) {
+    const client = await pool.connect();
+
+    try {
+        console.log('Connected to postgres database');
+        const res = await client.query(sql, params);
+        return res.rows;
+    } catch (err) {
+        console.error(err);
+    } finally {
+        // finally block is important to release the client from the pool so that it does not exhaust the resources of postgres. 
+        client.release();
+    }
+}
