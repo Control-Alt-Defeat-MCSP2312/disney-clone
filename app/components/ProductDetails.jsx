3c7c52f7a949e519330abb74c7cf004aeb81b822
DANIEL_GRADY <anakria977@gmail.com>
2024-03-05 18:26:03 -0500

    Add small changes to styling

--- app/components/ProductDetails.jsx
+++ app/components/ProductDetails.jsx
@@ -3,22 +3,22 @@ export default function ProductDetails() {
         <div className="product__details_background border-b-0">
             <div className="product__details_container border-b border-white mx-10">
                 <div className="product-details display: block m-0">
-                    <div className="product-tabs flex justify-between max-w-full pb-4 border-b-2 border-white my-10">
-                        <button className="product-tabs__tab_active display: block position: relative w-auto mt-0 no-underline">
+                    <div className="product-tabs flex justify-between max-w-full p-0 pb-4 border-b-4 border-slate-100 my-10">
+                        <button className="product-tabs__tab_active display: block position: relative w-auto mt-0 no-underline cursor-pointer">
                             <h2 className="product-tabs__header flex flex-row justify-center items-start p-0 mb-0 text-lg font-semibold font-sans leading-6">
                                 <div className="product-details-container display: block">
-                                    <span className="product__details text-base leading-6 tracking-wider uppercase font-bold">Product Details</span>
+                                    <span className="product__details text-base text-zinc-800 leading-6 tracking-wider uppercase font-bold">Product Details</span>
                                 </div>
                             </h2>
                         </button>
-                        <button className="product-tabs__tab_active display: block position: relative w-auto mt-0 no-underline">
+                        <button className="product-tabs__tab_active display: block position: relative w-auto mt-0 no-underline cursor-pointer">
                             <h2 className="product-tabs__header flex flex-row justify-center items-start p-0 mb-0 text-lg font-semibold font-sans leading-6">
                                 <div className="product-details-container display: block">
                                     <span className="product__details text-base leading-6 tracking-wider uppercase font-bold">Shipping & Delivery</span>
                                 </div>
                             </h2>
                         </button>
-                        <button className="product-tabs__tab_active display: block position: relative w-auto mt-0 no-underline">
+                        <button className="product-tabs__tab_active display: block position: relative w-auto mt-0 no-underline cursor-pointer">
                             <h2 className="product-tabs__header flex flex-row justify-center items-start p-0 mb-0 text-lg font-semibold font-sans leading-6">
                                 <div className="product-details-container display: block">
                                     <span className="product__details text-base leading-6 tracking-wider uppercase font-bold">Reviews</span>
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,16 @@ module.exports = {
       fontFamily: {
         sans: ['Avenir Next', 'Helvetica', 'Arial', 'sans-serif'],
       },
+      letterSpacing: {
+        tightest: '-.075em',
+        tighter: '-.05em',
+        tight: '-.025em',
+        normal: '0',
+        wide: '.025em',
+        wider: '.08em',
+        widest: '.1em',
+        widest: '.25em',
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
