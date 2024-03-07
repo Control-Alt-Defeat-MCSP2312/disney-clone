c5f9cc64b9507fee86f8fa0e8d0b1b223a50db3a
Matt Harlow <121141370+matt-harlow-9@users.noreply.github.com>
2024-03-07 17:29:46 -0500

    Update tailwind.config.js
    Duplicate custom gradient

--- tailwind.config.js
+++ tailwind.config.js
@@ -57,7 +57,6 @@ module.exports = {
       },
       backgroundImage: {
         'custom-gradient': 'linear-gradient(90deg, #0075e3 var(--percent), #cbe3f9 var(--percent))',
-        'custom-gradient': 'linear-gradient(90deg, #0075e3 var(--percent), #cbe3f9 var(--percent))',
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
