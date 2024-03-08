9f045380a81758a8826b8eb0471de276a01824b3
DANIEL_GRADY <anakria977@gmail.com>
2024-03-08 14:22:14 -0500

    Set functional layout for components in their respective containers

--- app/page.js
+++ app/page.js
@@ -5,22 +5,29 @@ import AdvertisementBanner from './components/AdvertisementBanner.jsx';
 import StickySidebar from './components/StickySidebar.jsx';
 import Header from './components/Header.jsx';
 import PrimaryImage from './components/PrimaryImage.jsx';
+import Footer from './components/Footer.jsx';
+import Shipping from './components/Shipping.jsx';
 import { ActiveProvider, ReviewProvider } from './context/context.jsx';
 
 export default function Home() {
   return (
-    <div className="flex flex-col h-screen antialiased">
+    <div className="antialiased">
       <Header />
-      <div className="flex flex-1">
-        <div className="flex flex-col flex-1">
-          <PrimaryImage />
+      <div className="pdp-detail max-w-full m-0 p-0 relative ">
+        <div className="product-detail-content-section relative flex flex-col">
+          <div className="product-detail-media-section block mr-424px relative">
+            <PrimaryImage />
+            <ReviewProvider>
+              <ActiveProvider>
+                <ProductDetails />
+              </ActiveProvider>
+            </ReviewProvider>
+          </div>
+          <div className="sticky-side-rail-content block w-424px bg-sticky-gradient z-899 absolute top-0 bottom-0 right-0 m-0 shadow-none">
+            <StickySidebar />
+          </div>
         </div>
-        <ReviewProvider>
-          <ActiveProvider>
-            <ProductDetails />
-          </ActiveProvider>
-        </ReviewProvider>
-        <StickySidebar />
+        <Shipping />
       </div>
       <AdvertisementBanner />
       <Footer />
