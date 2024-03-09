04ab26860b7b986988951f532d875971af01abe7
ObiFlo1 <rubflores1000@gmail.com>
2024-03-08 18:10:28 -0600

    Updated MTE

--- app/components/CarouselDetail.jsx
+++ app/components/CarouselDetail.jsx
@@ -1,17 +1,37 @@
-"use client";
-
 import React from "react";
+import { useState } from "react";
 
 export default function CarouselDetail({ product }) {
-  return (
-    <div className=" flex-col items-center justify-center mx-4">
-      <img className="item" src={product.src} />
+  const [hover, setHover] = useState(false);
 
-      <h2>{product.itemName}</h2>
-      <span className="flex gap-5 text-2xl px-4 font-extralight">
-        <p className="text-gray-900 line-through">{product.priceName}</p>
+  function handleClick() {
+    window.open(product.url, "_blank");
+  }
+
+  return (
+    <div className="flex flex-col items-center justify-center">
+      {/* Ensure flex column layout */}
+      <div className="flex items-center justify-center w-full mb-4">
+        <img
+          className="size-64"
+          src={product.src}
+          onMouseOver={() => setHover(true)}
+          onMouseOut={() => setHover(false)}
+          onClick={handleClick}
+        />
+      </div>
+      {hover ? (
+        <h2 className="hover: text-blue-500 transition ease-in-out">
+          {product.itemName}
+        </h2>
+      ) : (
+        <h2>{product.itemName}</h2>
+      )}
+      <div className="flex justify-between">
+        {/* Use a wrapper div for text */}
+        <p className="text-gray-900 line-through mr-2">{product.itemPrice}</p>
         <p className="text-red-500">{product.discounted}</p>
-      </span>
+      </div>
     </div>
   );
 }
