6576604dee7b5ef3f04c4849ac968a377ddb1553
FULL_NAME <EMAIL_ADDRESS>
2024-03-10 10:57:29 -0700

    reformatted main dropdown div

--- app/components/NavBar.jsx
+++ app/components/NavBar.jsx
@@ -34,44 +34,62 @@ function Navbar() {
   ];
 
   return (
-    <nav className="border">
-      <ul className="flex justify-between items-center font-avenir font-base font-normal w-full">
-        <div className="flex justify-center flex-grow">
+    <nav className="border p-5"> {/* Wraps entire nav bar */}
+      <ul className="flex justify-between items-center font-avenir font-base font-normal w-full"> {/* Container for nav bar content */}
+        <div className="flex justify-center flex-grow"> {/* wraps list items  */}
           {menu.map((category) => (
             <li key={category.name}
               onMouseEnter={() => setIsOpen(category.name)}
               onMouseLeave={() => setIsOpen(false)}
             >
-              <a href="#" className="relative text-sm w-fit block after:block after:content-[''] after:absolute after:h-[3px] after:bg-blue-500 after:w-full after:scale-x-0 hover:after:scale-x-100 after:transition after:duration-1000 after:origin-center">
+              <a href="#" className="relative text-sm w-fit block after:block after:content-[''] after:absolute after:h-[5px] after:bg-blue-500 after:w-full after:scale-x-0 hover:after:scale-x-100 after:transition after:duration-500 after:origin-center">
                 <button className="uppercase font-semibold text-sm hover:text-blue-500 leading-6 font-avenir">
                   {category.name}
                 </button>
               </a>
               {isOpen === category.name && (
-                <div className="absolute bg-white border-t border-gray-200 w-full left-0 right-0 z-10">
-                  <div className="flex">
-                    {/* Image container */}
-                    <div className="flex-none w-1/3 max-w-xs pr-7">
-                      {/* Make sure your image filenames match this logic */}
-                      <img src={`/36418_nt_20240205_2x.webp`} alt={category.name} />
-                    </div>
-                    {/* Subcategories Container */}
-                    <div className="flex justify-between w-2/3">
-                      {category.subcategories.map((subcategory, index) => (
-                        <div key={index} className="flex flex-col">
-                          <h3 className="font-bold text-lg mb-2">{subcategory.name}</h3>
-                          <ul className="space-y-2">
-                            {subcategory.items.map((item, idx) => (
-                              <li key={idx} className="hover:text-blue-500">
-                                {/* Make sure to replace this with your routing logic if needed */}
-                                <a href={`/categories/${item.toLowerCase()}`} className="block">{item}</a>
-                              </li>
-                            ))}
-                          </ul>
+                <div style={{
+                  maxHeight: '100vh',
+                  paddingTop: '40px',
+                  paddingBottom: '40px',
+                  paddingLeft: '30px',
+                  paddingRight: '30px',
+                  position: 'absolute',
+                  transition: 'max-height 1.2s',
+                  zIndex: 1200,
+                  lineHeight: '24px',
+                  display: 'flex',
+                }}>
+                  {category.subcategories.map((sub, index) => (
+                    <div key={index} className="flex flex-col px-4 py-2" style={{ /* Add individual styles here */ }}>
+                      <h3 className="font-bold text-lg mb-2">{sub.name}</h3>
+                      <hr className="mb-2" />
+                      <ul className="text-base list-none">
+                        {sub.items.map((item, idx) => (
+                          <li key={idx} className="py-1 hover:text-blue-500">
+                            {item !== 'null' ? item : ''}
+                          </li>
+                        ))}
+                      </ul>
+                      {/* Render additional categories for the last subcategory (Disney Park in this case) */}
+                      {index === category.subcategories.length - 1 && category.subcategories[index].additional && (
+                        <div className="mt-4">
+                          {category.subcategories[index].additional.map((additionalSub, additionalIdx) => (
+                            <div key={additionalIdx}>
+                              <h3 className="font-bold text-lg mb-2">{additionalSub.name}</h3>
+                              <ul className="text-base list-none">
+                                {additionalSub.items.map((additionalItem, itemIdx) => (
+                                  <li key={itemIdx} className="py-1 hover:bg-gray-100">
+                                    {additionalItem !== 'null' ? additionalItem : ''}
+                                  </li>
+                                ))}
+                              </ul>
+                            </div>
+                          ))}
                         </div>
-                      ))}
+                      )}
                     </div>
-                  </div>
+                  ))}
                 </div>
               )}
             </li>
@@ -83,4 +101,5 @@ function Navbar() {
 }
 
 export default Navbar;
+
 
